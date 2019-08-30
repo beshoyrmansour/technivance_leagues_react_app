aaf014f45f4eae753334cd6326fb10a7bf8fe193
Beshoy Mansour <beshoy.r.mansour@gmail.com>
2019-08-30 16:04:09 +0200

    add API_TOKEN

--- src/configs/APIs.js
+++ src/configs/APIs.js
@@ -1,5 +1,5 @@
 const baseURL = "https://api.football-data.org/v2";
-
+const API_TOKEN="bd3a4dceaf43410e83b1d3de50318d94";
 const API_ENDPOINTS = {
   ALL_LEAGUES: `${baseURL}/competitions`,
   LEAGUES_DETAILS: `${baseURL}/competitions/{LEAGUE_ID}`,
@@ -7,4 +7,4 @@ const API_ENDPOINTS = {
   LEAGUE_TEAM_DETAILS: `${baseURL}/competitions/{LEAGUE_ID}/teams/{TEAM_ID}`,
 };
 
-export default API_ENDPOINTS;
+export default {API_ENDPOINTS, API_TOKEN};
