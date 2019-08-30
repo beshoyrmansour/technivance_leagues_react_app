62a9d3d86a78199a4575abb1b0ea7c9f5217a232
Beshoy Mansour <beshoy.r.mansour@gmail.com>
2019-08-30 16:35:26 +0200

    Add routes and lazy load it by react-loadable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
+import "./App.css";
+import Loading from "./utils/Loading";
+import LeaguesPage from "./Pages/LeaguesPage";
 
 function App() {
+  // Pages
+  const LeagueDetails = Loadable({
+    loader: () => import("./Pages/LeagueDetails"),
+    loading: Loading
+  });
+  const TeamPage = Loadable({
+    loader: () => import("./Pages/TeamPage"),
+    loading: Loading
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/league/:league_id" name="Login Page" component={LeagueDetails} />
+        <Route exact path="/league/:league_id/team/:team_id" name="Login Page" component={TeamPage} />
+
+        <Route path="/" name="Forside" component={LeaguesPage} />
+      </Switch>
+    </BrowserRouter>
+    // <Route exact path="/404" name="Page 404" component={Page404} />
   );
 }
 
---
+++ src/Pages/LeagueDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeagueDetails = () => {
+    return (
+        <div>
+            LeagueDetails
+        </div>
+    )
+}
+
+export default LeagueDetails
