5e72c494df47c94f3530582a5bb542f52fab6286
Beshoy Mansour <beshoy.r.mansour@gmail.com>
2019-08-31 20:24:42 +0200

    Update Loading Comp

--- src/utils/Loading.jsx
+++ src/utils/Loading.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
-    return (
-        <div>
-            Loading
+  return (
+    <div className="mt-5">
+      <div className="loader-box">
+        <div className="loader-shadow"></div>
+        <div className="loader-gravity">
+          <div className="loader-ball"></div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
